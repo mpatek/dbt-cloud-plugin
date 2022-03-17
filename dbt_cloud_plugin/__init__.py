f8db8963cb5a5530fedd36ce3db04a6b54baddb4
Mike Patek <mpatek@gmail.com>
2022-03-17 14:53:34 -0400

    Update naming of sensor

--- dbt_cloud_plugin/__init__.py
+++ dbt_cloud_plugin/__init__.py
@@ -1,10 +1,10 @@
 from airflow.plugins_manager import AirflowPlugin
 from dbt_cloud_plugin.hooks.dbt_cloud_hook import DbtCloudHook
 from dbt_cloud_plugin.operators.dbt_cloud_run_job_operator import DbtCloudRunJobOperator
-from dbt_cloud_plugin.sensors.dbt_cloud_run_job_sensor import DbtCloudRunJobSensor
+from dbt_cloud_plugin.sensors.dbt_cloud_run_job_sensor import DbtCloudRunSensor
 
 class DbtCloudPlugin(AirflowPlugin):
     name = "dbt_cloud_plugin"
     operators = [DbtCloudRunJobOperator]
     hooks = [DbtCloudHook]
-    sensors = [DbtCloudRunJobSensor]
+    sensors = [DbtCloudRunSensor]
